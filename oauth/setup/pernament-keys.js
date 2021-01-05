457d28577f5782c472c4d10155141915e4a12fbf
bonito135 <500+bonito135@users.noreply.github.com>
2021-01-05 13:34:43 +0100

    ready to deploy

--- oauth/setup/pernament-keys.js
+++ oauth/setup/pernament-keys.js
@@ -3,6 +3,7 @@ module.exports = {
     clientID: "543ccf3b8d0a42ada0c87d95b10bd39f",
     clientSecret: "7e9b6c69718f4b7293fe31e6f6ae08b4",
     redirectURI: "http://localhost:5000/spotify/auth/redirect",
-    scope: "user-read-private user-read-email user-read-playback-state",
+    scope:
+      "streaming user-read-private user-read-email user-read-playback-state",
   },
 };
