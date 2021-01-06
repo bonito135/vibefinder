ccf649f3ba5747d84ce103e9848324a412b9ce67
bonito135 <500+bonito135@users.noreply.github.com>
2021-01-06 21:11:39 +0100

    added heroku url

--- oauth/setup/pernament-keys.js
+++ oauth/setup/pernament-keys.js
@@ -1,8 +1,10 @@
+const redirectTo = process.env.REDIRECT_TO || "http://localhost:5000";
+
 module.exports = {
   spotify: {
     clientID: "543ccf3b8d0a42ada0c87d95b10bd39f",
     clientSecret: "7e9b6c69718f4b7293fe31e6f6ae08b4",
-    redirectURI: "http://localhost:5000/spotify/auth/redirect",
+    redirectURI: `${redirectTo}/spotify/auth/redirect`,
     scope:
       "streaming user-read-private user-read-email user-read-playback-state",
   },
