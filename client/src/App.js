26f38355b630849f91c75484cec800961f2b16b4
bonito135 <500+bonito135@users.noreply.github.com>
2021-01-06 20:51:46 +0100

    maybe now.

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,14 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import logo from "./Spotify-Logo.svg";
 import "./App.css";
 
 function App() {
+  const redirectTo = process.env.REDIRECT_TO || "http://localhost:5000";
   const [currentSong, setCurrentSong] = useState("");
   const [currentArtist, setCurrentArtist] = useState("");
 
   const getCurrentlyPlaying = async () => {
-    const response = await fetch(
-      "http://localhost:5000/spotify/functions/playing"
-    );
+    const response = await fetch(`${redirectTo}/spotify/functions/playing`);
 
     console.log(response);
     if (response.status === 401) {
@@ -24,34 +23,13 @@ function App() {
     }
   };
 
-  useEffect(() => {
-    (function () {
-      var cookies = document.cookie.split("; ");
-      for (var c = 0; c < cookies.length; c++) {
-        var d = window.location.hostname.split(".");
-        while (d.length > 0) {
-          var cookieBase =
-            encodeURIComponent(cookies[c].split(";")[0].split("=")[0]) +
-            "=; expires=Thu, 01-Jan-1970 00:00:01 GMT; domain=" +
-            d.join(".") +
-            " ;path=";
-          var p = window.location.pathname.split("/");
-          document.cookie = cookieBase + "/";
-          while (p.length > 0) {
-            document.cookie = cookieBase + p.join("/");
-            p.pop();
-          }
-          d.shift();
-        }
-      }
-    })();
-  }, []);
-
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <a href="http://localhost:5000/spotify/auth/login">login</a>
+        <a href="https://currently-playing-spotify-song.herokuapp.com/spotify/auth/login">
+          login
+        </a>
 
         <div>
           <button onClick={getCurrentlyPlaying}>Currently playing</button>
