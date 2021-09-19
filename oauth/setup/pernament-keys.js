465f30c4b4b48c8ea5bd89183675cda82f4b655a
bonito135 <kubamedivision@gmail.com>
2021-09-19 18:31:21 +0200

    edited private keys for production

--- oauth/setup/pernament-keys.js
+++ oauth/setup/pernament-keys.js
@@ -8,8 +8,8 @@ if (process.env.CURRENT_URL) {
 
 module.exports = {
   spotify: {
-    clientID: "543ccf3b8d0a42ada0c87d95b10bd39f",
-    clientSecret: "7e9b6c69718f4b7293fe31e6f6ae08b4",
+    clientID: process.env.CLIENT_ID,
+    clientSecret: process.env.CLIENT_SECRET,
     redirectURI: `${currentURL}/spotify/auth/redirect`,
     scope:
       "streaming user-read-private user-read-email user-read-playback-state user-modify-playback-state",
